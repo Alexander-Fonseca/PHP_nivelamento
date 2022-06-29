bc373707798b7d02949f7683724f5ef9f9e5fb1f
Alexander Fonseca <motoleco@hotmail.com>
2022-06-28 22:29:12 -0300

    arrays

---
+++ arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+// $cursos = ['PHP', 'Python', 'JS',1 , 1.22];
+
+// var_dump($cursos);
+
+// $curso = [
+//   'nome' => 'PHP',
+//   'versao' => 8.1
+// ];
+// var_dump($curso);
+
+$cursos = [
+  0 => [
+    'nome' => 'PHP',
+    'versao' => 8.1
+  ],
+  1 => [
+    'nome' => 'Python',
+    'versao' => 5.1
+  ],
+  2 => [
+    'nome' => 'JS',
+    'versao' => 3.1
+  ],
+];
+
+var_dump($cursos);
